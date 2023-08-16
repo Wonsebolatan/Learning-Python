51672e9171d90248fc6da3ee38d4a1e157f40ee0
Ibrahim W Bashiru <ibrahim-bash@hotmail.co.uk>
2023-08-16 04:52:23 +0100

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,9 @@
+"""
+We check in a patient named John Smith.
+He's 20 years old and is a new patient.
+define three variable for his name, age and whether he is a new or an existing patient
+"""
+
+patient_name = 'John Smith'
+patient_age  = 20
+new_patient  = True
