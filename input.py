0fcee59cc1a1532a63cf2dc0480252d6ba7e48ff
Ibrahim W Bashiru <ibrahim-bash@hotmail.co.uk>
2023-08-16 04:49:47 +0100

    Add files via upload

---
+++ input.py
@@ -0,0 +1,8 @@
+"""
+Ask two questions: person's name and favourite colour.
+Then, print a message like "Mosh likes blue"
+"""
+
+name = input('What is your name? ')
+colour = input('What is your favourite colour? ')
+print(name + ' likes ' + colour)
