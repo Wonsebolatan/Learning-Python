6e7456a843f834db99222d52d0b7ba1548f93e34
Ibrahim W Bashiru <ibrahim-bash@hotmail.co.uk>
2023-08-20 20:27:08 +0100

    Add files via upload

---
+++ Comparison Operators.py
@@ -0,0 +1,18 @@
+"""
+if name is less than 3 characters long
+    name must be at least 3 characters
+otherwise if it's more than 50 characters long
+    name can be a maximum of 50 characters
+otherwise
+    name looks good
+"""
+name = input('What is your name? ')
+
+if len(name) < 3:
+    print("Name must be at least 3 characters.")
+elif len(name) > 50:
+    print("Name must be a maximum of 50 characters.")
+else:
+    print("Name looks good!")
+
+print(len(name))
