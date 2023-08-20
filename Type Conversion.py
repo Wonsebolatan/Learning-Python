9a26e80507c5b021a472bc2d2620bf9a5a24cae1
Ibrahim W Bashiru <ibrahim-bash@hotmail.co.uk>
2023-08-20 06:33:57 +0100

    Add files via upload

---
+++ Type Conversion.py
@@ -0,0 +1,9 @@
+"""
+Ask a user their weight (in pounds), convert it to
+kilograms and print on the terminal.
+"""
+
+weight_in_lbs = input(' What is your weight in pounds? ')
+weight_in_kg =  int(weight_in_lbs) * 0.45
+
+print(' Your weight in kilogram is',weight_in_kg,'kg')
